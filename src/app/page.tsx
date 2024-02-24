81f078243e78056d70465505208669abfa135814
shirosmith <shirosmith1793@gmail.com>
2024-02-24 16:20:40 +0900

    create portfolio page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,278 @@
+import TimelineItem from "@/components/atoms/TimelineItem";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-gray-200 p-4 min-h-screen flex flex-col items-center">
+      <h1 className="text-3xl mb-4">Smith Portfolio</h1>
+      {/* 自己紹介 */}
+      <div className="md:max-w-[80%] mx-auto w-full mb-4 bg-white shadow-md rounded-md overflow-hidden">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Shiro Mise</div>
+          <p className="text-gray-700 text-sm">
+            ウェブ開発が専門でフロントエンドからバックエンドまで幅広く経験を積んできました。<br />
+            プロジェクトに取り組む際には、クライアントの要望に真摯に向き合い、満足いただけるソリューションを提供することを心がけております。
+          </p>
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">Pros</div>
+            <ul className="text-sm">
+              <li>機能開発</li>
+              <li>データ設計</li>
+              <li>UI/UX改善</li>
+            </ul>
+          </div>
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">Hobby</div>
+            <ul className="text-sm">
+              <li>サウナ</li>
+              <li>ゲーム</li>
+              <li>筋トレ</li>
+            </ul>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Skill Set */}
+      <div className="md:max-w-[80%] mx-auto w-full mb-4 bg-white shadow-md rounded-md overflow-hidden">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Skill Set</div>
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">Language</div>
+            <ul className="text-sm">
+              <li>JavaScript</li>
+              <li>TypeScript</li>
+              <li>PHP</li>
+              <li>Ruby</li>
+              <li>HTML</li>
+              <li>CSS</li>
+            </ul>
+          </div>
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">Framework & Library</div>
+            <ul className="text-sm">
+              <li>Vue.js(Nuxt.js)</li>
+              <li>React.js(Next.js)</li>
+              <li>jQuery</li>
+              <li>Laravel</li>
+              <li>CakePHP</li>
+              <li>Ruby on Rails</li>
+            </ul>
+          </div> 
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">Database</div>
+            <ul className="text-sm">
+              <li>MySQL</li>
+              <li>PostgreSQL</li>
+              <li>Firestore</li>
+            </ul>
+          </div>
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">Infrastructure</div>
+            <ul className="text-sm">
+              <li>AWS</li>
+              <li>Sakura VPS</li>
+              <li>Heroku</li>
+              <li>Vercel</li>
+              <li>Netfily</li>
+            </ul>
+          </div>
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">Other tool</div>
+            <ul className="text-sm">
+              <li>Docker</li>
+              <li>Nginx</li>
+              <li>Apache</li>
+              <li>Swagger</li>
+            </ul>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* Projects */}
+      <div className="md:max-w-[80%] mx-auto w-full bg-white shadow-md rounded-md overflow-hidden">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Project History</div>
+        </div>
+        <ol className="relative ml-4 border-s border-gray-200 dark:border-gray-700">                  
+            <TimelineItem
+              title="電動キックボードシェアリングサービス"
+              tasks={[
+                "Vue2からVue3への移行",
+                "管理画面のUI/UX改善",
+                "データ設計",
+                "不具合改修"
+              ]}
+              skills={[
+                "Vue.js(Nuxt.js)",
+                "CSS(scss, bem)",
+                "Firebase(FireStore, CloudStrage, Authentication)"
+              ]}
+              time="April, 2024"
+              isLatest={true}
+            />
+            <TimelineItem
+              title="ナイトワークのキャスト向けのサービス"
+              tasks={[
+                "コンテンツの一部を新プロジェクトに移行",
+                "キャストのプロフィール機能のリニューアル",
+                "データ設計",
+                "UI/UX改善",
+                "不具合改修"
+              ]}
+              skills={[
+                "TypeScript(Next.js)",
+                "JavaScript(jQuery)",
+                "PHP(Laravel, CakePHP)",
+                "CSS(scss, tailwindcss)",
+                "MySQL"
+              ]}
+              time="April, 2024"
+              isLatest={true}
+            />
+            <TimelineItem
+              title="オンライン英会話サービス"
+              tasks={[
+                "新機能開発",
+                "Vue2からVue3への移行",
+                "データ設計",
+                "UI/UX改善",
+                "不具合改修"
+              ]}
+              skills={[
+                "TypeScript(Next.js, Nuxt.js)",
+                "Ruby(Ruby on Rails)",
+                "Protocol Buffer",
+                "CSS(scss, Vuetify)",
+                "MySQL"
+              ]}
+              time="April, 2023"
+            />
+            <TimelineItem
+              title="児童向けオンライン学習サービス"
+              tasks={[
+                "UI/UX改善",
+                "APIのドキュメント化",
+                "不具合改修"
+              ]}
+              skills={[
+                "TypeScript(Nuxt.js, CanvaJS)",
+                "PHP(Laravel)",
+                "CSS(scss, Vuetify)",
+                "PostgreSQL"
+              ]}
+              time="October, 2022"
+            />
+            <TimelineItem
+              title="インフルエンサーを活用した商品宣伝サービス"
+              tasks={[
+                "Pringを利用した決済機能の実装",
+                "TrustDogを利用した本人確認機能の実装",
+                "データ設計",
+                "UI/UX改善",
+                "ソースコードレビュー"
+              ]}
+              skills={[
+                "JavaScript(Vue.js, Nuxt.js, jQuery)",
+                "PHP(Laravel)",
+                "CSS(scss, Quasar, Ant)",
+                "MySQL"
+              ]}
+              time="Feburary, 2022"
+            />
+            <TimelineItem
+              title="地方企業を対象とした情報共有プラットフォームサービス"
+              tasks={[
+                "認証機能をAWS(Cognito)へ移行",
+                "インフラ構築のドキュメント作成",
+                "新機能開発",
+                "ソースコードレビュー"
+              ]}
+              skills={[
+                "TypeScript(Next.js)",
+                "PHP(Laravel)",
+                "CSS(scss, Ant, material-ui)",
+                "MySQL"
+              ]}
+              time="July, 2021"
+            />
+            <TimelineItem
+              title="イベント関連のプラットフォームサービス"
+              tasks={[
+                "オンラインチケット販売機能の実装",
+                "Tencent Cloudを利用した動画配信機能の実装",
+                "イベント会場のGoogleMapタイル画像生成機能の実装",
+                "独自ドメイン機能の実装",
+                "不具合改善",
+                "データ設計",
+                "ソースコードレビュー"
+              ]}
+              skills={[
+                "JavaScript(Nuxt.js, jQuery)",
+                "PHP(Laravel)",
+                "CSS(scss, Vuetify)",
+                "MySQL",
+                "Firebase(Firestore)"
+              ]}
+              time="Feburary, 2021"
+            />
+            <TimelineItem
+              title="アイスクリーム販売店の広告サービス"
+              tasks={[
+                "通知処理のクエリ改善",
+                "不具合改善",
+              ]}
+              skills={[
+                "JavaScript(jQuery)",
+                "PHP(CakePHP)",
+                "MySQL",
+              ]}
+              time="November, 2018"
+            />
+        </ol>
+      </div>
+      {/* Works */}
+      <div className="md:max-w-[80%] my-4 mx-auto w-full mb-4 bg-white shadow-md rounded-md overflow-hidden">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Works</div>
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">E-Neta-Houko</div>
+            <p className="text-sm mb-2">URL: <a className="underline" href="https://e-neta-houko.com" target="_blank">https://e-neta-houko.com</a></p>
+            <p className="mb-2 text-xs text-gray-400">ネットで見かけるネタ画像の元ネタまとめサービス</p>
+          </div>
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">Emovee</div>
+            <p className="text-sm mb-2">URL: <a className="underline" href="https://emovee.org" target="_blank">https://emovee.org</a></p>
+            <p className="mb-2 text-xs text-gray-400">Youtubeの動画リストを作成してシェアするサービス</p>
+          </div>
+        </div>
+      </div>
+      {/* Blog */}
+      <div className="md:max-w-[80%] my-4 mx-auto w-full mb-4 bg-white shadow-md rounded-md overflow-hidden">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Blog</div>
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">Shiro Secret Base</div>
+            <p className="text-sm mb-2">URL: <a className="underline" href="https://shiro-secret-base.com" target="_blank">https://shiro-secret-base.com</a></p>
+            <p className="mb-2 text-xs text-gray-400">技術ブログ</p>
+          </div>
+        </div>
+      </div>
+      {/* Contact */}
+      <div className="md:max-w-[80%] my-4 mx-auto w-full mb-4 bg-white shadow-md rounded-md overflow-hidden">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Contact</div>
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">Email</div>
+            <p className="text-sm mb-2">shirosmith1793アットgmail.com</p>
+            <p className="mb-2 text-xs text-gray-400">(アットを@に変換)</p>
+          </div>
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">X(Twitter)</div>
+            <p className="text-sm mb-2 underline"><a href="https://twitter.com/smith39871365" target="_blank">smith39871365</a></p>
+          </div>
+          <div className="my-4">
+            <div className="text-md mb-2 border-b">Github</div>
+            <p className="text-sm mb-2 underline"><a href="https://github.com/smithshiro" target="_blank">smithshiro</a></p>
+          </div>
+        </div>
       </div>
     </main>
   );
---
+++ src/components/atoms/TimelineItem.tsx
@@ -0,0 +1,58 @@
+type Props = {
+  title: string
+  skills: string[]
+  tasks: string[]
+  time: string
+  isLatest?: boolean
+}
+const TimelineItem: React.FC<Props> = (data: Props) => {
+  const {
+    title,
+    skills,
+    tasks,
+    description,
+    time,
+    isLatest = false
+  } = data
+
+  return (
+    <li className="mb-10 ms-6">            
+        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+        </span>
+        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+          {title}
+          {
+            isLatest ? (
+              <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">Latest</span>
+            ) : null
+          }
+        </h3>
+        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{time}</time>
+        <div className="my-2">
+          <p className="border-b">Tasks</p>
+          <ul className="mb-2 text-sm text-gray-400">
+            {
+              tasks.map((task, index) => {
+                return (<li key={index}>{task}</li>)
+              })
+            }
+          </ul>
+        </div>
+        <div className="my-2">
+          <p className="border-b">Skill Set</p>
+          <ul className="flex flex-wrap gap-x-2 text-sm text-gray-400">
+            {
+              skills.map((skill, index) => {
+                return (<li key={index}>{skill}</li>)
+              })   
+            }
+          </ul>
+        </div> 
+    </li>
+  )
+}
+
+export default TimelineItem
