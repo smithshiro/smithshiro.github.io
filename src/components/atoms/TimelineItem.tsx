5134b6c770082fce800b28824499d11c78bac798
shirosmith1793 <shirosmith1793@gmail.com>
2025-06-22 07:53:48 +0900

    Update TimelineItem component with dark mode support

--- src/components/atoms/TimelineItem.tsx
+++ src/components/atoms/TimelineItem.tsx
@@ -5,6 +5,7 @@ type Props = {
   time: string
   isLatest?: boolean
 }
+
 const TimelineItem: React.FC<Props> = (data: Props) => {
   const {
     title,
@@ -16,7 +17,7 @@ const TimelineItem: React.FC<Props> = (data: Props) => {
 
   return (
     <li className="mb-10 ms-6">            
-        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 dark:bg-blue-900 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900">
             <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
             </svg>
@@ -31,8 +32,8 @@ const TimelineItem: React.FC<Props> = (data: Props) => {
         </h3>
         <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{time}</time>
         <div className="my-2">
-          <p className="border-b">Tasks</p>
-          <ul className="mb-2 text-sm text-gray-400">
+          <p className="border-b border-gray-200 dark:border-gray-600 text-gray-900 dark:text-white">Tasks</p>
+          <ul className="mb-2 text-sm text-gray-600 dark:text-gray-400">
             {
               tasks.map((task, index) => {
                 return (<li key={index}>{task}</li>)
@@ -41,8 +42,8 @@ const TimelineItem: React.FC<Props> = (data: Props) => {
           </ul>
         </div>
         <div className="my-2">
-          <p className="border-b">Skill Set</p>
-          <ul className="flex flex-wrap gap-x-2 text-sm text-gray-400">
+          <p className="border-b border-gray-200 dark:border-gray-600 text-gray-900 dark:text-white">Skill Set</p>
+          <ul className="flex flex-wrap gap-x-2 text-sm text-gray-600 dark:text-gray-400">
             {
               skills.map((skill, index) => {
                 return (<li key={index}>{skill}</li>)
